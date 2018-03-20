2162e78c29901476411e63da406e9428c7f09b83
Kailin Cannon <kailin.cannon@gmail.com>
2018-03-20 17:49:13 -0600

    Hey you know that thing where the quote at the bottom was changing every time you clicked a button? It doesn't do that anymore. Congrats.

--- src/ScrollText/ScrollText.js
+++ src/ScrollText/ScrollText.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './scroll-text.css';
 
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      films: this.props.films,
+      randomFilm: this.props.films[Math.floor(Math.random()*this.props.films.length)]
+    }
+  }
 
-const Sidebar = (props) => {
-
-  const randomFilmText =
-    props.films[Math.floor(Math.random()*props.films.length)].opening_crawl
-
-
-  return (
-    <div className="sidebar">
-      <marquee className="story-text"
-               behavior="scroll"
-               direction="left"
-               scrollamount="15">
-        {randomFilmText}
-      </marquee>
-    </div>
-  )
-
-
+  render(props) {
+    return (
+      <div className="sidebar">
+        <marquee className="story-text"
+                 behavior="scroll"
+                 direction="left"
+                 scrollamount="10">
+          {this.state.randomFilm.opening_crawl}
+        </marquee>
+        <div className="movie-details">
+        {this.state.randomFilm.title} ({this.state.randomFilm.release_date.slice(0,4)})
+        </div>
+      </div>
+    )
+  }
 }
 
 
